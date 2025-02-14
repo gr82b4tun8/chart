0d577d3f84c592ae1c266ded7007f0440d13af24
gr82b4tun8 <gr82b4tun8@gmail.com>
2025-02-14 13:08:27 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const chart = LightweightCharts.createChart(document.getElementById('chart'), {
+    width: 760,
+    height: 400,
+    layout: {
+        backgroundColor: '#ffffff',
+        textColor: '#000',
+    },
+    grid: {
+        vertLines: {
+            color: 'rgba(0, 0, 0, 0.05)',
+        },
+        horzLines: {
+            color: 'rgba(0, 0, 0, 0.05)',
+        },
+    },
+    priceScale: {
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+    },
+    timeScale: {
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        timeVisible: true,
+    },
+});
